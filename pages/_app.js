68528ebbcd3860c38710b9b49f6f0536d368214e
Poodit <surjv86o2@gmail.com>
2025-06-25 10:00:54 +0700

    เพิ่ม Google Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,27 @@
-import '@/styles/globals.css'
+// pages/_app.js
+import Script from 'next/script';
+import '../styles/globals.css';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      {/* Google Analytics */}
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=G-TPH1VDFXZW`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-TPH1VDFXZW');
+        `}
+      </Script>
+
+      <Component {...pageProps} />
+    </>
+  );
 }
+
+export default MyApp;
